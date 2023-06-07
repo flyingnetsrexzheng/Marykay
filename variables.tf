927dba780aed61b9048253088ff7047dac9bbdd2
rexzheng <rex_zheng@flyingnets.com>
2023-06-07 15:12:19 +0800

    variables add

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
+# variables for AWS_ACCESS_KEY_ID
+variable "AWS_ACCESS_KEY_ID" {
+  type    = string
+  description = "import from terraform cloud"
+}
+
+# variables for AWS_SECRET_ACCESS_KEY
+variable "AWS_SECRET_ACCESS_KEY" {
+  type    = string
+  description = "import from terraform cloud"
+}
+
+
 # variables for EC2 secret key
 variable "EC2-key-pair" {
   type    = string
